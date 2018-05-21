0cac921694692dc4a7b0fcf297b0f542da338ea5
Nghia Diep <nghiadieppham@gmail.com>
2018-05-21 17:10:04 +0700

    Replace Duc-san code and update common page title fix

--- resources/views/root/notices.blade.php
+++ resources/views/root/notices.blade.php
@@ -5,27 +5,28 @@
 <div class="notice-page">
 
     <h2 class="page-title">EATASからのお知らせ</h2>
-    <div class="block-accordion">
-        <dl class="block-accordion__section">
-            @foreach($notices as $notice)
-            <dt class="block-accordion__title">
-                <span class="block-accordion__date">
-                    @userdate($notice->publish_date)
-                </span>
-                <span class="block-accordion__title__des">
-                    {{$notice->title}}
-                </span>
-            </dt>
-            <dd class="block-accordion__content">
-                <p>
-                    @lined($notice->description)
-                </p>
-            </dd>
-            @endforeach
-        </dl>
-        <!--/.block-accordion__section-->
+    <div class="content-center">
+        <div class="block-accordion">
+            <dl class="block-accordion__section">
+                @foreach($notices as $notice)
+                <dt class="block-accordion__title">
+                    <span class="block-accordion__date">
+                        @userdate($notice->publish_date)
+                    </span>
+                    <span class="block-accordion__title__des">
+                        {{$notice->title}}
+                    </span>
+                </dt>
+                <dd class="block-accordion__content">
+                    <p>
+                        @lined($notice->description)
+                    </p>
+                </dd>
+                @endforeach
+            </dl>
+            <!--/.block-accordion__section-->
+        </div>
+        <!--/.block-accordion-->
     </div>
-    <!--/.block-accordion-->
-
+    @endsection
 </div>
-@endsection
